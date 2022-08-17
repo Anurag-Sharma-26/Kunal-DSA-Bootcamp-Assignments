ce7042a0b7d4493d415fa7369192ed0960ac3f02
Wildy 26 <anurag182001sharma@gmail.com>
2022-08-17 23:30:06 +0530

    Armstrong and Reverse String program added.

---
+++ 03-conditionals-loops/Intermediate Java Programs Solutions/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the word to check: ");
+        String str = scan.next();
+        String reverseStr = "";
+
+        int strLength = str.length();
+
+        for(int i = (strLength - 1); i >= 0; --i){
+            reverseStr += str.charAt(i);
+        }
+        System.out.println(reverseStr);
+    }
+}
